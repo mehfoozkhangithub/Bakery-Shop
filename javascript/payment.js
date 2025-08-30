fb527d829c788e0ba61c0f2085a3ae25e7e952d2
SirajAhmedShekh <sirajshekh068@gmail.com>
2025-08-30 17:06:14 +0530

    paymentcode

---
+++ javascript/payment.js
@@ -0,0 +1,25 @@
+function paymentFunc(e) {
+    e.preventDefault();
+    let userName = document.getElementById("user");
+    let userEmail = document.getElementById("email");
+    let address = document.getElementById("address");
+    let city = document.getElementById("city");
+    let state = document.getElementById("state");
+    let zipCode = document.getElementById("zipCode");
+    let cardName = document.getElementById("cardName");
+    let cardNumber = document.getElementById("cardNumber");
+    let expMonth = document.getElementById("expMonth");
+    let expYear = document.getElementById("expYear");
+    let cardCvv = document.getElementById("cardCvv");
+
+// ==== user validation =====
+
+
+
+
+
+
+
+
+
+}
